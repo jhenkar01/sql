5553da41832dd681e4ad58ae987bc66644c9628b
unknown <jhenkar.xworkz@gmail.com>
2024-06-27 00:25:34 +0530

    create table cricket

---
+++ createtable.sql
@@ -0,0 +1,3 @@
+SELECT * FROM xworkz1_btm.airport_details;
+use xworkz1_btm
+CREATE TABLE Cricket_teams( id int, team_name varchar(20), no_of_players int, captain varchar(20), coach varchar(20) )cricket_teams
